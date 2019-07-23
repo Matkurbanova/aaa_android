c6f96923610588e4988162dad86f88ffc95ca997
Urmat <urma.93.kg@gmail.com>
2019-07-23 20:31:07 +0600

    Initial commit

---
+++ app/src/main/java/kg/itrun/android/aaa/MainActivity.java
@@ -0,0 +1,13 @@
+package kg.itrun.android.aaa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
