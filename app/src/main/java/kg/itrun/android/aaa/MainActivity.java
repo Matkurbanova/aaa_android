b1e764464527ce6e33f115ddd60b4db413abbe53
Urmat <urma.93.kg@gmail.com>
2019-07-27 18:10:04 +0600

    Migrate to AndroidX

--- app/src/main/java/kg/itrun/android/aaa/MainActivity.java
+++ app/src/main/java/kg/itrun/android/aaa/MainActivity.java
@@ -1,6 +1,6 @@
 package kg.itrun.android.aaa;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
