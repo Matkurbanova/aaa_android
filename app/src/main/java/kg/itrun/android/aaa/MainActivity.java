937f0f7eec4aab8c18a90c1d38220473232cb6ac
Urmat <urma.93.kg@gmail.com>
2019-08-03 16:49:48 +0600

    new Menu

--- app/src/main/java/kg/itrun/android/aaa/MainActivity.java
+++ app/src/main/java/kg/itrun/android/aaa/MainActivity.java
@@ -1,25 +1,16 @@
 package kg.itrun.android.aaa;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.view.View;
-
-import androidx.core.view.GravityCompat;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-
+import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.view.Menu;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -60,7 +51,14 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-
+        switch (item.getItemId()) {
+            case R.id.nav_categories:
+                System.out.println("Categories");
+                break;
+            case R.id.nav_news:
+                System.out.println("NEWS");
+                break;
+        }
         return true;
     }
 }
