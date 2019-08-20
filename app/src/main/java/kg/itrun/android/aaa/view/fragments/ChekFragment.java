b3e2e7b470ea0c4620f0cb019172f2df8ac51eaf
Ali <alinur1003@gmail.com>
2019-08-20 21:13:38 +0600

    ChekFragment

---
+++ app/src/main/java/kg/itrun/android/aaa/view/fragments/ChekFragment.java
@@ -0,0 +1,31 @@
+package kg.itrun.android.aaa.view.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import kg.itrun.android.aaa.R;
+
+public class ChekFragment extends Fragment {
+    private ChekFragment chekFragment;
+    private RecyclerView recyclerView;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.chek_fragment, container, false);
+        initViews(view);
+        return view;
+    }
+    private void initViews( View view){
+
+    }
+
+}
+
+
