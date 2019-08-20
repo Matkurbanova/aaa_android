033100ad0b36c2a289e9f1b33152dadcfebff098
Bermet <bermetmatkurbanova@gmail.com>
2019-08-20 19:23:38 +0600

    Favorite 2

--- app/src/main/java/kg/itrun/android/aaa/view/fragments/FavoriteFragment.java
+++ app/src/main/java/kg/itrun/android/aaa/view/fragments/FavoriteFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -33,6 +34,8 @@ public class FavoriteFragment extends Fragment {
         favoriteAdapter = new FavoriteAdapter(getContext());
 
         recyclerViewFavorite.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerViewFavorite.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
         recyclerViewFavorite.setAdapter(favoriteAdapter);
         favoriteAdapter.setFavoriteList(DataGen.genFavorite(35));
     }
